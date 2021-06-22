99bc8a407a09b627eeb6c0395a5dca79ff8cbb98
Weverton Icaro <weverton.dev@gmail.com>
2021-06-22 11:43:02 -0300

    [Aula02] Create user Admin

---
+++ README.md
@@ -0,0 +1,23 @@
+# NLW 06 - Valoriza
+
+## Regras
+
+- Cadastro de usuários
+
+  [] Email obrigátorio nos cadastros.
+
+  [] Cadastro único por email.
+
+- Cadastro de Tags
+
+  [] Proibido duplicata de Tags.
+
+  [] Não é permitido cadastro de Tags sem nome.
+
+  [] Cadastro de Tags é feito apenas por administradores.
+
+- Cadastro de elogios
+
+  [] Usários apenas podem elogiar outros usuários.
+
+  [] Elogios só podem ser efetuados por usuários autenticados.
---
+++ src/entities/User.ts
@@ -0,0 +1,38 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { v4 as uuid } from 'uuid';
+
+@Entity('users')
+export class User {
+  @PrimaryColumn()
+  readonly id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  admin: boolean;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  constructor() {
+    //Create new user.id
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
+}
+
+export default User;
